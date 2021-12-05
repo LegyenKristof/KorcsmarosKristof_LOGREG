36457c206cc83e90e168ef97141294b128e915db
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-05 04:15:05 +0100

    LoggedInActivity kinézet kész

--- app/src/main/java/com/example/logreg/LoggedInActivity.java
+++ app/src/main/java/com/example/logreg/LoggedInActivity.java
@@ -2,13 +2,35 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+    private TextView textViewL1;
+    private Button buttonL1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        init();
+
+        buttonL1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoggedInActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void init(){
+        textViewL1 = findViewById(R.id.textViewL1);
+        buttonL1 = findViewById(R.id.buttonL1);
     }
 }
