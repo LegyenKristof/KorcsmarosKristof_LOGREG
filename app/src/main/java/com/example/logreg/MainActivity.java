f7e5b2b4f8045f5070d6f645f7f52615c068d143
LegyenKristof <48032365+LegyenKristof@users.noreply.github.com>
2021-12-05 03:47:48 +0100

    MainActivity kinézet kész

--- app/src/main/java/com/example/logreg/MainActivity.java
+++ app/src/main/java/com/example/logreg/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText1, editText2;
+    private Button button1, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void init(){
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
     }
 }
